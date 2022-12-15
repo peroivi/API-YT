2e2caff16d2cda176237ab980670886cb8fb1e7f
Pere_Roca <73195339+peroivi@users.noreply.github.com>
2022-12-15 20:42:49 +0100

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -48,7 +48,7 @@ For more information about the client_secrets.json file format, please visit:
 https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                                    CLIENT_SECRETS_FILE))
-
+#valors possibles privadesa videos
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
 # valida els credencials
