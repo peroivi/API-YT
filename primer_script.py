9203a49efdb8b92889d44e1becfdf39f731f4972
Pere_Roca <73195339+peroivi@users.noreply.github.com>
2022-12-15 20:27:30 +0100

    Update primer_script.py

--- primer_script.py
+++ primer_script.py
@@ -1,12 +1,12 @@
 from googleapiclient.discovery import build
 
-api_key = 'AIzaSyCQnghJSvTkupqj9vMbs6SdESD50lQYDEk'
+api_key = 'key-publica-api'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 request = youtube.channels().list(
     part='statistics',
-    id='UC4PIwddtPs6p3asO433oMdQ'
+    id='id-canal'
 )
 
 response = request.execute()
